1d9a6a11f9b55d815acacc0bd3a71a6447fa8604
Saverio Miroddi <saverio.pub2@gmail.com>
2017-12-22 11:30:34 +0100

    Make client name pipe write non blocking

--- spotlight_client.rb
+++ spotlight_client.rb
@@ -36,10 +36,17 @@ class SpotlightClient
   def send_command(command)
     case command
     when COMMAND_SHOW, COMMAND_QUIT
-      IO.write(FIFO_FILENAME, command)
+      write_to_named_pipe(FIFO_FILENAME, command)
     else
       raise "Unexpected command: #{command.inspect}"
     end
+  end
+
+  def write_to_named_pipe(filename, content)
+    # Non-blocking write. In case there is no reader on the other side of the pipe, at least this
+    # process won't hang.
+    # File::RDWR aliases `w+`; both don't make the nonblocking nature explicit.
+    open(filename, File::RDWR) { |f| f.puts(content) }
   end
 end
 
