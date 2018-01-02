1f8eb4df5af2d970cf81b9c6e1bcf0bcd95eee23
Saverio Miroddi <saverio.pub2@gmail.com>
2018-01-02 22:18:35 +0100

    Messaging::Sender#send_message is now thread-safe
    Only one message at a time will be sent.

--- lib/pm_spotlight_daemon/messaging/sender.rb
+++ lib/pm_spotlight_daemon/messaging/sender.rb
@@ -1,3 +1,5 @@
+require 'thread'
+
 module PmSpotlightDaemon
   module Messaging
     # See Receiver documentation.
@@ -7,15 +9,20 @@ module PmSpotlightDaemon
 
       def initialize(writer)
         @writer = writer
+        @sending_semaphore = Mutex.new
       end
 
+      # Thread-safe; will send only an message at a time.
+      #
       def send_message(message)
         raise "Terminator (#{TERMINATOR.inspect}) is not accepted in messages" if message.include?(TERMINATOR)
 
         encoded_message = message + TERMINATOR
 
-        @writer.write(encoded_message)
-        @writer.flush
+        @sending_semaphore.synchronize do
+          @writer.write(encoded_message)
+          @writer.flush
+        end
       end
     end
   end
